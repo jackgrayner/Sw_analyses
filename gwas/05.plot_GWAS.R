b78276ac51d0fb6fa6d2786da8f7b1b808b45e88
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2026-03-29 12:37:48 -0400

    Update 05.plot_GWAS.R

--- gwas/05.plot_GWAS.R
+++ gwas/05.plot_GWAS.R
@@ -113,3 +113,37 @@ Sw.plot.zoomed<-ggplot(Sw[Sw$chr==1 & Sw$ps>96e+06 & Sw$ps<246e+06,],aes(x=ps/1e
   scale_colour_manual(values=c("#ccc","#db6767"))+
   geom_point(size=1)+scale_y_continuous(limits=c(1,13.5),expand=c(0,0))+
   ylab("-log10(P)")
+
+
+
+#
+#R version 4.4.2 (2024-10-31)
+#Platform: aarch64-apple-darwin20
+#Running under: macOS Sonoma 14.3
+
+#Matrix products: default
+#BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib 
+#LAPACK: /Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/lib/libRlapack.dylib;  LAPACK version 3.12.0
+
+#locale:
+#[1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+
+#time zone: America/New_York
+#tzcode source: internal
+
+#attached base packages:
+#[1] stats4    stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+# [1] ggridges_0.5.6       ggbeeswarm_0.7.2     lubridate_1.9.4      forcats_1.0.0        purrr_1.0.4          readr_2.1.5         
+# [7] tibble_3.2.1         tidyverse_2.0.0      tidyr_1.3.1          patchwork_1.3.0      ggplot2_4.0.2        dplyr_1.1.4         
+#[13] stringr_1.5.1        GenomicRanges_1.56.2 GenomeInfoDb_1.40.1  IRanges_2.38.1       S4Vectors_0.42.1     BiocGenerics_0.50.0 
+
+#loaded via a namespace (and not attached):
+# [1] gtable_0.3.6            jsonlite_1.8.9          compiler_4.4.2          tidyselect_1.2.1        scales_1.4.0           
+# [6] R6_2.5.1                XVector_0.44.0          generics_0.1.3          GenomeInfoDbData_1.2.12 tzdb_0.4.0             
+#[11] pillar_1.10.1           RColorBrewer_1.1-3      rlang_1.1.5             stringi_1.8.4           S7_0.2.1               
+#[16] timechange_0.3.0        cli_3.6.3               withr_3.0.2             magrittr_2.0.3          zlibbioc_1.50.0        
+#[21] grid_4.4.2              rstudioapi_0.17.1       hms_1.1.3               beeswarm_0.4.0          lifecycle_1.0.4        
+#[26] vipor_0.4.7             vctrs_0.6.5             glue_1.8.0              farver_2.1.2            httr_1.4.7             
+#[31] tools_4.4.2             pkgconfig_2.0.3         UCSC.utils_1.0.0       
